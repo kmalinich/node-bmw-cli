b3ae11b25811e2afe96bc8f411e2a761c8b4b7d9
kdm <kennygprs@gmail.com>
2017-10-04 22:32:28 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+/* eslint no-global-assign: "off", no-console: "off" */
+
+app_path = __dirname;
+app_name = 'bmwcli';
+app_intf = 'cli';
+
+process.title = app_name;
+
+// node-bmw libraries
+json   = require('json');
+log    = require('log-output');
+socket = require('socket');
+update = require('update');
+
+
+// Configure term event listeners
+function term_config(pass) {
+	process.on('SIGTERM', () => {
+		console.log('');
+		log.msg({ msg : 'Caught SIGTERM' });
+		process.nextTick(term);
+	});
+
+	process.on('SIGINT', () => {
+		console.log('');
+		log.msg({ msg : 'Caught SIGINT' });
+		process.nextTick(term);
+	});
+
+	process.on('exit', () => {
+		log.msg({ msg : 'Terminated' });
+	});
+
+	process.nextTick(pass);
+}
+
+// Global init
+function init() {
+	log.msg({ msg : 'Initializing' });
+
+	json.read(() => { // Read JSON config and status files
+		socket.init(() => { // Start zeroMQ client
+			log.msg({ msg : 'Initialized' });
+		}, term);
+	}, term);
+}
+
+// Global term
+function term() {
+	log.msg({ msg : 'Terminating' });
+
+	socket.term(() => { // Stop socket.io client
+		json.write(() => { // Write JSON config and status files
+			process.exit();
+		});
+	});
+}
+
+// FASTEN SEATBELTS
+term_config(() => {
+	init();
+});
